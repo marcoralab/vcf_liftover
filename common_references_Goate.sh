7c0ef86ac0a0f7241ff35d8c807b22cf5f1090f1
Brian Fulton-Howard <fultonh1@gmail.com>
2022-09-01 18:40:06 -0400

    Update common_references_Goate.sh

--- common_references_Goate.sh
+++ common_references_Goate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rf data/ref
-mkdir -p data/ref
-cp -rs  /sc/arion/projects/LOAD/Data/liftover_pipeline_data/* data/ref/
+rm -rf resources/ref
+mkdir -p resources/ref
+cp -rs  /sc/arion/projects/LOAD/Data/liftover_pipeline_data/* resources/ref/
 
